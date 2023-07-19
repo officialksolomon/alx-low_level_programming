e878e424df63302f5efca78e85772b61afdb8330
officialksolomon <solomonuche42@gmail.com>
2023-07-18 19:58:22 -0700

    #include main.h
    /**
     * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
     * followed by a new line
     */
    void print_alphabet_x10(void)
    {
    char ch;
    int i;
    
    i = 0;
    
    while (i < 10)
    {
    ch = 'a';
    while (ch <= 'z')
    {
    _putchar(ch);
    ch++;
    }
    _putchar('\n');
    i++;
    }
    }

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * using a nested loop, followed by a new line
+ * Return: 0 if successful
+ */
+void print_alphabet_x10(void)
+{
+	char character;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		character = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(character);
+			character++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
