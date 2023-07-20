42ed19ebdff59f8ba197e8eb5326c6d7ed12d0a8
officialksolomon <solomonuche42@gmail.com>
2023-07-19 20:28:17 -0700

    0-main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - Tests the function positive_or_negative
+* Return: 0 if succesful,non-zer otherwise.
+*/
+
+int main(void)
+{
+	int i;
+
+	i = -5;
+	positive_or_negative(i);
+
+	return (0);
+}
