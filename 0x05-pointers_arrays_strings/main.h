d0efe4639deeb67d0e7104f59331a4115472f9d6
officialksolomon <solomonuche42@gmail.com>
2023-07-24 09:10:33 -0700

    0 reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+void _puts(char *str);
+int _strlen(char *s);
+void _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
+
