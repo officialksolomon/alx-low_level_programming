241be0b4feed643fad213388e1a3bfd92943eead
officialksolomon <solomonuche42@gmail.com>
2023-07-24 14:06:48 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to standard output (stdout).
+ * @str: pointer to  string to print.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
