669213856a346c4ec7e30ac71289eaa82407f34f
officialksolomon <solomonuche42@gmail.com>
2023-07-31 11:18:15 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Reademe for 0x07-pointers_arrays_strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+void print_diagsums(int *a, int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strstr(char *haystack, char *needle);
+void set_string(char **s, char *to);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
+#endif
