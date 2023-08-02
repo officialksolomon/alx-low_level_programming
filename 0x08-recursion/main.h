ae6dbe0c2f9a9039a02d6a0c83cfdcccfbd83387
officialksolomon <solomonuche42@gmail.com>
2023-08-02 14:32:52 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int is_palindrome(char *s);
+void _putchar(char c);
+int _pow_recursion(int x, int y);
+int _sqrt(int n, int i);
+int _sqrt_recursion(int n);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+int factorial(int n);
+int _strlen_recursion(char *s);
+int is_prime_number(int n);
+int wildcmp(char *s1, char *s2);
+
+#endif
+
