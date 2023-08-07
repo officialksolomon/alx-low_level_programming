10305cbb4c58359c3a381778dcffc4a6d5535892
officialksolomon <solomonuche42@gmail.com>
2023-08-08 00:21:46 +0100

    static libraries.

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character in
+ * the given text.
+ * @c: the char to be checked.
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	_puts("\"The best of the best\"\n\t- Solomon Uche");
+	return (0);
+}
